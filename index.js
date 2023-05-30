59e5934722bb24ce0464fca3880eb3665203771f
Ahmet-Cavus <130047754+Ahmet-Cavus@users.noreply.github.com>
2023-05-30 16:49:43 +0300

    Add files via upload
    30.05.2023

--- index.js
+++ index.js
@@ -3,25 +3,29 @@
 // Başlangıç Challenge'ı
 
 /**Örnek Görev: İlkini Dön
- * 
+ *
  * Bu örnek sonradan gelecek olan görevleri nasıl çözeceğinizi size gösterecek.
- * 
+ *
  * Aşağdıaki Yüksek dereceden fonskiyonu(higher-order function) kullanarak aşağıdakileri yapınız
  *  1. Stringlerden oluşan bir array'i parametre olarak alın
- *  2. Bir string'i değişken olarak alan bir callback fonksiyonunu parametre olarak alın 
+ *  2. Bir string'i değişken olarak alan bir callback fonksiyonunu parametre olarak alın
  *  3. Array'in İLK elemanını değişken olarak alarak çalışacak olan callback fonksiyonunun sonucunu dönün
- * 
+ *
  * Aşağıdaki kodlar bu görevin nasıl yapılacağına örnek olacaktır
  * Bu fonskiyon 'asas' dönmeli(return)
-*/
+ */
 
 function ilkiniDon(stringArray, callback) {
-  return callback(stringArray[0])
+  return callback(stringArray[0]);
 }
-console.log('örnek görev:', ilkiniDon(['as','sa'],function(metin){return metin+metin}));
+console.log(
+  "örnek görev:",
+  ilkiniDon(["as", "sa"], function (metin) {
+    return metin + metin;
+  })
+);
 
 // Başlangıç Challenge'ı Sonu
-
 
 ///// M V P ///////
 
@@ -30,18 +34,27 @@ console.log('örnek görev:', ilkiniDon(['as','sa'],function(metin){return metin
   Aşağıdaki skor1 ve skor2 kodlarını inceleyiniz ve aşağıdaki soruları altına not alarak cevaplayın
   
   1. skor1 ve skor2 arasındaki fark nedir?
+  Skor1 ve Skor2 arasındaki temel fark, Skor1'in bir closure kullanmasıdır. skor1 bir fonksiyon döndürürken, 
+  Skor2 doğrudan bir fonksiyon olarak tanımlanmıştır.
   
   2. Hangisi bir closure kullanmaktadır? Nasıl tarif edebilirsin? (yarınki derste öğreneceksin :) )
+  Skor1 bir closure kullanmaktadır. Closure, bir iç fonksiyonun, dış fonksiyonun kapsamındaki değişkenlere erişim 
+  sağlaması durumudur. skorArtirici() fonksiyonu, içinde tanımlanan skor değişkenine iç fonksiyon skorGuncelle() 
+  tarafından erişim sağlar. Bu nedenle, skor1 bir closure örneğidir. Closure, iç fonksiyonun dış fonksiyonunun 
+  kapsamındaki değişkenleri "kapsar" ve sonradan bu değişkenlere erişebilir.
   
   3. Hangi durumda skor1 tercih edilebilir? Hangi durumda skor2 daha mantıklıdır?
+  Skor1, skor değişkenini koruma altına alarak, skoru sadece skorGuncelle() fonksiyonu aracılığıyla güncelleyebilir. 
+  Bu durumda, skor1 tercih edilebilir çünkü dışarıdan skor değişkenine erişim kısıtlanır ve sadece belirlenen fonksiyon 
+  aracılığıyla güncellenebilir.
 */
 
 // skor1 kodları
 function skorArtirici() {
   let skor = 0;
   return function skorGuncelle() {
-   return skor++;
-  }
+    return skor++;
+  };
 }
 
 const skor1 = skorArtirici();
@@ -53,7 +66,6 @@ function skor2() {
   return skor++;
 }
 
-
 /* Görev 2: takimSkoru() 
 Aşağıdaki takimSkoru() fonksiyonununda aşağıdakileri yapınız:
   1. Bir çeyrekte bir takımın ürettiği skoru rastgele(random) elde eden bir sonuc dönünüz(return)
@@ -64,12 +76,11 @@ Aşağıdaki takimSkoru() fonksiyonununda aşağıdakileri yapınız:
 Not: Bu fonskiyon, aşağıdaki diğer görevler için de bir callback fonksiyonu olarak da kullanılacak
 */
 
-function takimSkoru(/*Kodunuzu buraya yazınız*/){
-    /*Kodunuzu buraya yazınız*/
+function takimSkoru() {
+  return 10 + Math.floor(Math.random() * 16);
 }
 
-
-
+console.log(takimSkoru());
 
 /* Görev 3: macSonucu() 
 Aşağıdaki macSonucu() fonksiyonununda aşağıdakileri yapınız:
@@ -84,16 +95,23 @@ Aşağıdaki macSonucu() fonksiyonununda aşağıdakileri yapınız:
   "EvSahibi": 92,
   "KonukTakim": 80
 }
-*/ 
+*/
 
-function macSonucu(/*Kodunuzu buraya yazınız*/){
-  /*Kodunuzu buraya yazınız*/
+function macSonucu(takimSkoruCallBack, ceyrekSayisi) {
+  let evSahibiSkor = 0;
+  let konukTakimSkor = 0;
+
+  for (let i = 0; i < ceyrekSayisi; i++) {
+    evSahibiSkor += takimSkoruCallBack();
+    konukTakimSkor += takimSkoruCallBack();
+  }
+  return {
+    EvSahibi: evSahibiSkor,
+    KonukTakim: konukTakimSkor,
+  };
 }
 
-
-
-
-
+console.log(macSonucu(takimSkoru, 4));
 
 /* Zorlayıcı Görev 4: periyotSkoru()
 Aşağıdaki periyotSkoru() fonksiyonununda aşağıdakileri yapınız:
@@ -108,12 +126,18 @@ Aşağıdaki periyotSkoru() fonksiyonununda aşağıdakileri yapınız:
 }
   */
 
+function periyotSkoru(takimSkoru) {
+  const EvSahibi = takimSkoru("EvSahibi");
+  const KonukTakim = takimSkoru("KonukTakim");
 
-function periyotSkoru(/*Kodunuzu buraya yazınız*/) {
-  /*Kodunuzu buraya yazınız*/
-
+  return {
+    EvSahibi: EvSahibi,
+    KonukTakim: KonukTakim,
+  };
 }
 
+const skorlar = periyotSkoru(takimSkoru);
+console.log(skorlar);
 
 /* Zorlayıcı Görev 5: skorTabelasi() 
 Aşağıdaki skorTabelasi() fonksiyonunu kullanarak aşağıdakileri yapınız:
@@ -146,17 +170,48 @@ MAÇ UZAR ise skorTabelasi(periyotSkoru,takimSkoru,4)
 ] */
 // NOTE: Bununla ilgili bir test yoktur. Eğer logladığınız sonuçlar yukarıdakine benziyor ise tmamlandı sayabilirsiniz.
 
-function skorTabelasi(/*Kodunuzu buraya yazınız*/) {
-  /*Kodunuzu buraya yazınız*/
+function skorTabelasi(periyotSkoruCallBack, takimSkoruCallBack, ceyrekSayisi) {
+  const sonuc = [];
+  let evSahibiSkor = 0;
+  let konukTakimSkor = 0;
+
+  for (let i = 0; i < ceyrekSayisi; i++) {
+    const periyot = periyotSkoruCallBack(takimSkoruCallBack);
+    sonuc.push(
+      `${i + 1}. Periyot: Ev Sahibi ${periyot.EvSahibi} - Konuk Takim ${
+        periyot.KonukTakim
+      }`
+    );
+    evSahibiSkor += periyot.EvSahibi;
+    konukTakimSkor += periyot.KonukTakim;
+  }
+
+  if (evSahibiSkor === konukTakimSkor) {
+    let i = 0;
+    while (evSahibiSkor === konukTakimSkor) {
+      i++;
+      const periyot = periyotSkoruCallBack(takimSkoruCallBack);
+      sonuc.push(
+        `${i}. Uzatma: Ev Sahibi ${periyot.EvSahibi} - Konuk Takim ${periyot.KonukTakim}`
+      );
+      evSahibiSkor += periyot.EvSahibi;
+      konukTakimSkor += periyot.KonukTakim;
+    }
+  }
+
+  sonuc.push(
+    `Maç Sonucu: Ev Sahibi ${evSahibiSkor} - Konuk Takim ${konukTakimSkor}`
+  );
+
+  return sonuc;
 }
 
-
-
+console.log(skorTabelasi(periyotSkoru, takimSkoru, 4));
 
 /* Aşağıdaki satırları lütfen değiştirmeyiniz*/
-function sa(){
-  console.log('Kodlar çalışıyor');
-  return 'as';
+function sa() {
+  console.log("Kodlar çalışıyor");
+  return "as";
 }
 sa();
 module.exports = {
@@ -168,4 +223,4 @@ module.exports = {
   macSonucu,
   periyotSkoru,
   skorTabelasi,
-}
+};
